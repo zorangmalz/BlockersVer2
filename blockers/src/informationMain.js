76fd72ca2d75bf8e9a5d193f121cd63c173d469f
CEOJINSUNG <CEOJINSUNG@.github.com>
2020-09-01 15:12:24 +0900

    informationMain.js 추가

---
+++ blockers/src/informationMain.js
@@ -0,0 +1,201 @@
+import React, {useState, useEffect} from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Image,
+} from 'react-native';
+
+const information = StyleSheet.create({
+    largeText: {
+        fontSize: 24,
+        fontFamily: 'NunitoSans-Bold',
+        alignSelf: 'center',
+        color: '#303030',
+        marginTop: 16,
+        marginBottom: 16
+    },
+    mediumText: {
+        fontSize: 18,
+        fontFamily: 'NunitoSans-Regular',
+        color: '#303030',
+        opacity: 0.8
+    },
+    buttonBox: {
+        width: "80%",
+        height: 50,
+        borderRadius: 28,
+        borderWidth: 1,
+        borderColor: '#999999',
+        marginLeft: 38,
+        marginRight: 38,
+        marginBottom: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    activeButton: {
+        width: "80%",
+        height: 50,
+        borderRadius: 28,
+        backgroundColor: '#FFB83D',
+        marginLeft: 38,
+        marginRight: 38,
+        marginBottom: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default function InformationMain({ navigation }) {
+    const quesone = "금연 리포트"
+    const questwo = "복약 등록"
+    const questhree = "금연 자료실"
+    const quesfour = "금연 지원 사업 정보"
+    const [one, setOne] = useState(false);
+    const [two, setTwo] = useState(false);
+    const [three, setThree] = useState(false);
+    const [four, setFour] = useState(false);
+    const [select, setSelect] = useState([]);
+    const [clear, setClear] = useState(false);
+    var count = 4;
+
+    const pushone = () => {
+        setSelect(select.concat(quesone));
+        setTimeout(() => {
+            navigation.navigate('InformationNonReport');
+        }, 200)
+    }
+
+    const filterone = () => {
+        setSelect(select.filter(info => info !== quesone))
+    }
+
+    const pushtwo = () => {
+        setSelect(select.concat(questwo));
+    }
+
+    const filtertwo = () => {
+        setSelect(select.filter(info => info !== questwo))
+    }
+
+    const pushthree = () => {
+        setSelect(select.concat(questhree));
+    }
+
+    const filterthree = () => {
+        setSelect(select.filter(info => info !== questhree))
+    }
+
+    const pushfour = () => {
+        setSelect(select.concat(quesfour));
+    }
+
+    const filterfour = () => {
+        setSelect(select.filter(info => info !== quesfour))
+    }
+
+    useEffect(() => {
+        one === true ? count = count + 1 : count = count - 1;
+        two === true ? count = count + 1 : count = count - 1;
+        three === true ? count = count + 1 : count = count - 1;
+        four === true ? count = count + 1 : count = count - 1;
+        if ((count <= 2) && (count >= 0)) {
+            setClear(true);
+            console.log(select)
+        } else {
+            if(select[0]==="금연 리포트") setOne(false);
+            if(select[0]==="복약 등록") setTwo(false);
+            if(select[0]==="금연 자료실") setThree(false);
+            if(select[0]==="금연 지원 사업 정보") setFour(false);
+            setSelect(select.slice(1, select.length));
+            console.log(select);
+            setClear(false);
+        }
+    }, [one, two, three, four]);
+    return (
+        <>
+            <StatusBar barStyle="light-content" />
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#ffffff' }}>
+                <View accessibilityRole="header" style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 50, width: "100%", paddingLeft: "5%", paddingRight: "5%" }}>
+                    <View
+                        style={{
+                            height: 44,
+                            flexDirection: 'row',
+                            paddingTop: 4,
+                            justifyContent: "flex-start",
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Text style={{ fontSize: 24 }}>
+                            <Text style={{ fontFamily: 'NunitoSans-Bold', color: '#5CC27B' }}>Information</Text>
+                        </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <TouchableOpacity style={{ marginLeft: 8 }}>
+                            <Image source={require('./icon/alram.png')} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <ScrollView>
+                <Text style={information.largeText}>금연에 필요한 모든 것</Text>
+                    {one === false ?
+                        <TouchableOpacity onPressIn={pushone} onPress={()=>setOne(!one)}>
+                            <View style={information.buttonBox}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#303030' }]}>{quesone}</Text>
+                            </View>
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity onPressIn={filterone} onPress={()=>setOne(!one)}>
+                            <View style={information.activeButton}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#FFFFFF' }]}>{quesone}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                    {two === false ?
+                        <TouchableOpacity onPressIn={pushtwo} onPress={()=>setTwo(!two)}>
+                            <View style={information.buttonBox}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#303030' }]}>{questwo}</Text>
+                            </View>
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity onPressIn={filtertwo} onPress={()=>setTwo(!two)}>
+                            <View style={information.activeButton}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#FFFFFF' }]}>{questwo}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                    {three === false ?
+                        <TouchableOpacity onPressIn={pushthree} onPress={()=>setThree(!three)}>
+                            <View style={information.buttonBox}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#303030' }]}>{questhree}</Text>
+                            </View>
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity onPressIn={filterthree} onPress={()=>setThree(!three)}>
+                            <View style={information.activeButton}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#FFFFFF' }]}>{questhree}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                    {four === false ?
+                        <TouchableOpacity onPressIn={pushfour} onPress={()=>setFour(!four)}>
+                            <View style={information.buttonBox}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#303030' }]}>{quesfour}</Text>
+                            </View>
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity onPressIn={filterfour} onPress={()=>setFour(!four)}>
+                            <View style={information.activeButton}>
+                                <Text style={[information.mediumText, { fontFamily: 'NunitoSans-Bold', color: '#FFFFFF' }]}>{quesfour}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    )
+}
