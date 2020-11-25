0354bdf63233eedc998122e37bad99a05dddafe3
CEOJINSUNG <CEOJINSUNG@.github.com>
2020-11-25 12:27:38 +0900

    Revert "성공후기 12번"
    This reverts commit 42c0278ddcb7593041f247d2c60c80109923114d.

--- blockers/src/Challenge/ChallengeScreen.js
+++ blockers/src/Challenge/ChallengeScreen.js
@@ -1268,7 +1268,7 @@ export function ChallengeRegister({ navigation }) {
             title: "성공 후기 작성하기",
             content: "금연 성공한 후 느낀점, 다른사람에게 하고싶은말, 지지자에게 고마움을 표현해 보세요",
             period: "final",
-            id: 12,
+            id: 13,
             navigate: "ChallengeSuccess",
             visible:false,
             stats:false,
@@ -1868,7 +1868,7 @@ export function ChallengeMission({ navigation, route }) {
     const Final = [
         {
             number: 1,
-            title: "12. 성공 후기 작성하기",
+            title: "1. 성공 후기 작성하기",
             content: "금연 성공한 후 느낀점, 다른사람에게 하고싶은말, 지지자에게 고마움을 표현해 보세요",
         },
     ]
