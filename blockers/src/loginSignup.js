025e4f8fafb0f07421136acf42abfc1c8bb28cd1
CEOJINSUNG <CEOJINSUNG@.github.com>
2020-11-26 14:05:32 +0900

    login

--- blockers/src/loginSignup.js
+++ blockers/src/loginSignup.js
@@ -236,33 +236,35 @@ export default function LoginSignup({ navigation }) {
     }
   }
 
-  const [produce, setProduce] = useState(false)
   async function EmailVeri() {
-    await auth().currentUser.reload().then(() => {
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          console.log(user.emailVerified)
-          if (user.emailVerified) {
-            setEmailLoading(false)
-            console.log('User account created & signed in!');
-            navigation.navigate("프로필 설정")
-          } else  {
-            setEmailLoading(false)
-            Alert.alert(
-              "이메일 인증",
-              "이메일 인증이 되지 않았습니다.",
-              [
-                {
-                  text: "확인",
-                  onPress: () => user.delete()
-                }
-              ]
-            )
-          }
+    await auth().currentUser.reload()
+    await firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        console.log(user.emailVerified)
+        if (user.emailVerified) {
+          setEmailLoading(false)
+          console.log('User account created & signed in!');
+          navigation.navigate("프로필 설정")
         } else {
-          console.log("사용자가 없음")
+          setEmailLoading(false)
+          Alert.alert(
+            "이메일 인증",
+            "이메일 인증이 되지 않았습니다.",
+            [
+              {
+                text: "취소",
+                onPress: () => console.log("취소")
+              },
+              {
+                text: "확인",
+                onPress: () => user.delete()
+              }
+            ]
+          )
         }
-      })
+      } else {
+        console.log("사용자가 없음")
+      }
     })
   }
 
@@ -347,64 +349,64 @@ export default function LoginSignup({ navigation }) {
       <StatusBar barStyle="light-content" />
       <SafeAreaView style={{ flex: 1, backgroundColor: '#ffffff' }}>
         {emailLoading ?
-        <>
-          <Text style={{ fontFamily: "NunitoSans-Regular", fontSize: 16, position:"absolute", top: HEIGHT/2-40, alignSelf: "center", color: "#303030"}}>3분 이내 이메일 인증 완료해주세요</Text>
-          <TouchableOpacity onPress={EmailVeri} style={{height: 30, backgroundColor: "#5cc27b", position: "absolute", top: HEIGHT/2, alignSelf: "center", paddingLeft: 10, paddingRight: 10, borderRadius: 8}}><Text style={{fontFamily: "NunitoSans-Bold", fontSize: 16, color: "#ffffff"}}>인증 완료</Text></TouchableOpacity>
-        </>
-        :
-        <>
-        <View accessibilityRole="header" style={{ flexDirection: 'row', alignItems: 'center', height: 50, paddingTop: 5, width: "100%", paddingLeft: "3%", paddingRight: "3%" }}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Ionicons name="chevron-back" size={25} />
-          </TouchableOpacity>
-          <View
-            style={{
-              height: 44,
-              flexDirection: 'row',
-              justifyContent: "flex-start",
-              alignItems: 'center',
-              marginLeft: 24
-            }}
-          >
-            <Text style={{ fontSize: 18 }}>
-              <Text style={{ fontFamily: 'NunitoSans-Bold', color: '#303030' }}>회원가입</Text>
-            </Text>
-          </View>
-        </View>
-        <ScrollView style={{ paddingTop: 20 }}>
-          <TextInput value={email} onChangeText={text => setEmail(text)} style={[login.textinput, { borderBottomColor: emailtouch }]} placeholder="이메일 주소" placeholderTextColor="#999999" />
-          <Text style={login.text}>{emailLong}</Text>
-          <TextInput value={newpassword} onChangeText={text => setNewpassword(text)} textContentType="newPassword" secureTextEntry={true} style={[login.textinput, { borderBottomColor: emailtouch }]} placeholder="비밀번호(영문, 숫자 포함 6자리)" placeholderTextColor="#999999" />
-          <Text style={login.text}>{passLong}</Text>
-          <TextInput value={password} onChangeText={text => setPassword(text)} textContentType="password" secureTextEntry={true} style={[login.textinput, { borderBottomColor: emailtouch }]} placeholder="비밀번호 확인" placeholderTextColor="#999999" />
-          <Text style={login.text}>{texts}</Text>
+          <>
+            <Text style={{ fontFamily: "NunitoSans-Regular", fontSize: 16, position: "absolute", top: HEIGHT / 2 - 40, alignSelf: "center", color: "#303030" }}>3분 이내 이메일 인증 완료해주세요</Text>
+            <TouchableOpacity onPress={EmailVeri} style={{ height: 30, backgroundColor: "#5cc27b", position: "absolute", top: HEIGHT / 2, alignSelf: "center", paddingLeft: 10, paddingRight: 10, borderRadius: 8 }}><Text style={{ fontFamily: "NunitoSans-Bold", fontSize: 16, color: "#ffffff" }}>인증 완료</Text></TouchableOpacity>
+          </>
+          :
+          <>
+            <View accessibilityRole="header" style={{ flexDirection: 'row', alignItems: 'center', height: 50, paddingTop: 5, width: "100%", paddingLeft: "3%", paddingRight: "3%" }}>
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Ionicons name="chevron-back" size={25} />
+              </TouchableOpacity>
+              <View
+                style={{
+                  height: 44,
+                  flexDirection: 'row',
+                  justifyContent: "flex-start",
+                  alignItems: 'center',
+                  marginLeft: 24
+                }}
+              >
+                <Text style={{ fontSize: 18 }}>
+                  <Text style={{ fontFamily: 'NunitoSans-Bold', color: '#303030' }}>회원가입</Text>
+                </Text>
+              </View>
+            </View>
+            <ScrollView style={{ paddingTop: 20 }}>
+              <TextInput value={email} onChangeText={text => setEmail(text)} style={[login.textinput, { borderBottomColor: emailtouch }]} placeholder="이메일 주소" placeholderTextColor="#999999" />
+              <Text style={login.text}>{emailLong}</Text>
+              <TextInput value={newpassword} onChangeText={text => setNewpassword(text)} textContentType="newPassword" secureTextEntry={true} style={[login.textinput, { borderBottomColor: emailtouch }]} placeholder="비밀번호(영문, 숫자 포함 6자리)" placeholderTextColor="#999999" />
+              <Text style={login.text}>{passLong}</Text>
+              <TextInput value={password} onChangeText={text => setPassword(text)} textContentType="password" secureTextEntry={true} style={[login.textinput, { borderBottomColor: emailtouch }]} placeholder="비밀번호 확인" placeholderTextColor="#999999" />
+              <Text style={login.text}>{texts}</Text>
 
-          {signUpState ?
-            <TouchableOpacity onPress={signup} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16 }]}>
-              <Text style={login.buttontext}>회원가입</Text>
-            </TouchableOpacity>
-            :
-            <TouchableOpacity activeOpacity={0.3} style={[login.notbuttonbox, { marginTop: 16 }]}>
-              <Text style={login.buttontext}>회원가입</Text>
-            </TouchableOpacity>
-          }
+              {signUpState ?
+                <TouchableOpacity onPress={signup} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16 }]}>
+                  <Text style={login.buttontext}>회원가입</Text>
+                </TouchableOpacity>
+                :
+                <TouchableOpacity activeOpacity={0.3} style={[login.notbuttonbox, { marginTop: 16 }]}>
+                  <Text style={login.buttontext}>회원가입</Text>
+                </TouchableOpacity>
+              }
 
-          <TouchableOpacity onPress={() => navigation.navigate('로그인')}>
-            <Text style={login.signtext}>이미 회원이신가요?</Text>
-          </TouchableOpacity>
-          <View style={{ width: "90%", height: 0.2, borderWidth: 0.2, borderColor: '#C6C6C6', alignSelf: 'center' }} />
-          {/* <TouchableOpacity onPress={onFacebookButtonPress} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16, backgroundColor: '#4a67ad' }]}>
+              <TouchableOpacity onPress={() => navigation.navigate('로그인')}>
+                <Text style={login.signtext}>이미 회원이신가요?</Text>
+              </TouchableOpacity>
+              <View style={{ width: "90%", height: 0.2, borderWidth: 0.2, borderColor: '#C6C6C6', alignSelf: 'center' }} />
+              {/* <TouchableOpacity onPress={onFacebookButtonPress} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16, backgroundColor: '#4a67ad' }]}>
             <Text style={login.buttontext}>Facebook으로 시작하기</Text>
           </TouchableOpacity> */}
-          <TouchableOpacity onPress={onGoogleButtonPress} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16, backgroundColor: '#c45545' }]}>
-            <Text style={login.buttontext}>Gmail로 시작하기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity isLoading={loginLoading} onPress={() => kakaoLogin()} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16, backgroundColor: '#f6e14b' }]}>
-            <Text style={[login.buttontext, { color: '#303030' }]}>Kakaotalk으로 시작하기</Text>
-          </TouchableOpacity>
-        </ScrollView>
-        </>
-}
+              <TouchableOpacity onPress={onGoogleButtonPress} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16, backgroundColor: '#c45545' }]}>
+                <Text style={login.buttontext}>Gmail로 시작하기</Text>
+              </TouchableOpacity>
+              <TouchableOpacity isLoading={loginLoading} onPress={() => kakaoLogin()} activeOpacity={0.3} style={[login.buttonbox, { marginTop: 16, backgroundColor: '#f6e14b' }]}>
+                <Text style={[login.buttontext, { color: '#303030' }]}>Kakaotalk으로 시작하기</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </>
+        }
       </SafeAreaView>
     </>
   )
